c1c6e1c26eabe6c9663f754505c1061f7a09e009
strangeal <bajerijustice@gmail.com>
2023-01-17 12:20:23 -0500

    add models

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
+  belongs_to :author, class_name: 'User'
+  before_save :posts_counter
+
+  def posts_counter
+    author.update(posts_counter: author.posts.all.length)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+end
