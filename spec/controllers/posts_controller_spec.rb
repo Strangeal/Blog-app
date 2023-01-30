1820aef0723ce0118364569e37e64f3329939a58
strangeal <bajerijustice@gmail.com>
2023-01-30 02:39:09 -0800

    Add database object in controller specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,35 +3,48 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :request do
   describe 'post controller' do
     context 'index action' do
+      subject { User.new(name: 'Bajeri') }
+      before { subject.save }
+      before(:each) { get "/users/#{subject.id}/posts/" }
+
       it 'render index action' do
-        get '/users/24/posts/'
         expect(response).to render_template(:index)
       end
 
       it 'return status code ok' do
-        get '/users/24/posts/'
         expect(response).to have_http_status(:ok)
       end
 
       it 'return index view text' do
-        get '/users/24/posts/'
         expect(response.body).to include('Posts index method')
       end
     end
 
     context 'show action' do
+      subject { User.new(name: 'Bajeri') }
+      before { subject.save }
+
       it 'render index action' do
-        get '/users/24/posts/123'
+        post = Post.new(author: subject, title: 'Hello, I’m Tom from Mexico and am a Teacher',
+                        text: 'This is my first post')
+        post.save!
+        get "/users/#{subject.id}/posts/#{post.id}"
         expect(response).to render_template(:show)
       end
 
       it 'return status code ok' do
-        get '/users/24/posts/123'
+        post = Post.new(author: subject, title: 'Hello, I’m Tom from Mexico and am a Teacher',
+                        text: 'This is my first post')
+        post.save!
+        get "/users/#{subject.id}/posts/#{post.id}"
         expect(response).to have_http_status(:ok)
       end
 
       it 'return show view text' do
-        get '/users/24/posts/123'
+        post = Post.new(author: subject, title: 'Hello, I’m Tom from Mexico and am a Teacher',
+                        text: 'This is my first post')
+        post.save!
+        get "/users/#{subject.id}/posts/#{post.id}"
         expect(response.body).to include('Posts show method')
       end
     end
