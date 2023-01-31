eb7b0ce0a52b604ff476d209390b7946d1510d0c
strangeal <bajerijustice@gmail.com>
2023-01-31 02:00:25 -0800

    Fix rubocop

--- spec/view/posts/index_spec.rb
+++ spec/view/posts/index_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Posts index view", type: :system do
-  describe "Index page" do
+RSpec.describe 'Posts index view', type: :system do
+  describe 'Index page' do
     before(:each) do
-      @first_user = User.create(name: 'Tom', photo: 'https://res.cloudinary.com/dxsom7jmx/image/upload/v1674721420/Meta%20tags/girl-2696947_1280_2_ct8ivd.jpg', bio: 'Teacher from Mexico.')
-      @second_user = User.create(name: 'Mike', photo: 'https://res.cloudinary.com/dxsom7jmx/image/upload/v1674721410/Meta%20tags/photo_2022-11-19_16.50.29_y1mzds.jpg', bio: 'Teacher from Algeria.')
-
-      @first_post = Post.create(author: @first_user, title: 'Hello, I’m Tom from Mexico and am a Teacher',  text: 'This is my first post')
-      @second_post = Post.create(author: @second_user, title: 'Hello, I’m Lilly from Poland and am a Student', text: 'This is my second post')
+      # rubocop:disable Layout/LineLength
+      @first_user = User.create(name: 'Tom', photo: 'https://res.cloudinary.com/dxsom7jmx/image/upload/v1674721420/Meta%20tags/girl-2696947_1280_2_ct8ivd.jpg', bio: 'Mexico.')
+      @second_user = User.create(name: 'Mike', photo: 'https://res.cloudinary.com/dxsom7jmx/image/upload/v1674721410/Meta%20tags/photo_2022-11-19_16.50.29_y1mzds.jpg', bio: 'Algeria.')
+      # rubocop:enable Layout/LineLength
+      @first_post = Post.create(author: @first_user, title: 'Hello, I’m Tom from Mexico and am a Teacher',
+                                text: 'This is my first post')
+      @second_post = Post.create(author: @second_user, title: 'Hello, I’m Lilly from Poland and am a Student',
+                                 text: 'This is my second post')
 
       Comment.create(post: @first_post, author: @first_user, text: 'Amazing post Tom')
       Comment.create(post: @first_post, author: @first_user, text: 'Awesome post Tom')
@@ -22,7 +25,7 @@ RSpec.describe "Posts index view", type: :system do
     end
 
     it 'Can see user\'s username' do
-      expect(page).to have_content("#{@first_user.name}")
+      expect(page).to have_content(@first_user.name.to_s)
     end
 
     it 'Can see number of posts the user has written' do
@@ -30,15 +33,15 @@ RSpec.describe "Posts index view", type: :system do
     end
 
     it 'Can see a post\'s title' do
-      expect(page).to have_content("#{@first_post.title}")
+      expect(page).to have_content(@first_post.title.to_s)
     end
 
     # it 'Can see some of the post\'s body' do
     #     expect(page).to have_content("#{@first_post.text[0..50]}")
     # end
-    
+
     it 'Can see the first comments on a post' do
-      expect(page).to have_content("#{@first_post.comments.last.text}")
+      expect(page).to have_content(@first_post.comments.last.text.to_s)
     end
 
     it 'Can see how many comments a post has' do
@@ -48,9 +51,9 @@ RSpec.describe "Posts index view", type: :system do
     it 'Can see how many likes a post has' do
       expect(page).to have_content("Likes: #{@first_post.likes_counter}")
     end
-    
+
     it 'Can see a pagination button if there are more posts than fit' do
-      expect(page).to have_content("Pagination")
+      expect(page).to have_content('Pagination')
     end
 
     it 'Can click on a post, to be redirected to post\'s show page' do
--- spec/view/users/show_spec.rb
+++ spec/view/users/show_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "User show view", type: :system do
+RSpec.describe 'User show view', type: :system do
   describe 'Show page' do
     before(:each) do
-      @first_user = User.create(name: 'Tom', photo: 'https://res.cloudinary.com/dxsom7jmx/image/upload/v1674721420/Meta%20tags/girl-2696947_1280_2_ct8ivd.jpg', bio: 'Teacher from Mexico.')
-      @second_user = User.create(name: 'Mike', photo: 'https://res.cloudinary.com/dxsom7jmx/image/upload/v1674721410/Meta%20tags/photo_2022-11-19_16.50.29_y1mzds.jpg', bio: 'Teacher from Algeria.')
-
-      @first_post = Post.create(author: @first_user, title: 'Hello, I’m Tom from Mexico and am a Teacher',  text: 'This is my first post')
-      @second_post = Post.create(author: @second_user, title: 'Hello, I’m Lilly from Poland and am a Student', text: 'This is my second post')
+      # rubocop:disable Layout/LineLength
+      @first_user = User.create(name: 'Tom', photo: 'https://res.cloudinary.com/dxsom7jmx/image/upload/v1674721420/Meta%20tags/girl-2696947_1280_2_ct8ivd.jpg', bio: 'Mexico.')
+      @second_user = User.create(name: 'Mike', photo: 'https://res.cloudinary.com/dxsom7jmx/image/upload/v1674721410/Meta%20tags/photo_2022-11-19_16.50.29_y1mzds.jpg', bio: 'Algeria.')
+      # rubocop:enable Layout/LineLength
+      @first_post = Post.create(author: @first_user, title: 'Hello, I’m Tom from Mexico and am a Teacher',
+                                text: 'This is my first post')
+      @second_post = Post.create(author: @second_user, title: 'Hello, I’m Lilly from Poland and am a Student',
+                                 text: 'This is my second post')
 
       @user = User.all
       visit user_path(@first_user.id)
     end
 
     it 'Can see user\'s profile picture' do
-        expect(page).to have_css("img[src*='#{@first_user.photo}']")
+      expect(page).to have_css("img[src*='#{@first_user.photo}']")
     end
 
     it 'Can see user\'s username' do
-      expect(page).to have_content("#{@first_user.name}")
+      expect(page).to have_content(@first_user.name.to_s)
     end
 
     it 'Can see number of posts by user' do
@@ -26,7 +29,7 @@ RSpec.describe "User show view", type: :system do
     end
 
     it 'Can see the user\'s bio' do
-      expect(page).to have_content("#{@first_user.bio}")
+      expect(page).to have_content(@first_user.bio.to_s)
     end
 
     it 'Can see the user\'s last 3 posts' do
@@ -37,7 +40,7 @@ RSpec.describe "User show view", type: :system do
     end
 
     it 'Can see a button that lets me view all of a user\'s posts' do
-      expect(page).to have_content("See all posts")
+      expect(page).to have_content('See all posts')
     end
 
     it 'Can click on a user\'s, to be redirected to that post\'s show page' do
@@ -50,5 +53,5 @@ RSpec.describe "User show view", type: :system do
       click_link 'See all posts'
       expect("#{page.current_url}/posts").to match user_posts_path(@first_user.id)
     end
-end
+  end
 end
