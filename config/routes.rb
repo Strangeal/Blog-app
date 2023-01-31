b8cf8ff368d005a3146b7a3a5c626ea29040f98f
strangeal <bajerijustice@gmail.com>
2023-01-31 04:23:07 -0800

    Remove commented codes

--- config/routes.rb
+++ config/routes.rb
@@ -11,20 +11,3 @@ Rails.application.routes.draw do
     end
   end
 end
-
-
-    # get "users/:id" => "users#show", as: :users_next
-
-    # get "users/:user_id/posts/" => "posts#index", as: :users_posts
-
-    # get "users/:user_id/posts/new" => "posts#new", as: :posts_new
-    # post "users/:user_id/posts/new" => "posts#create", as: :posts_create
-
-    # get "users/:user_id/posts/:id" => "posts#show", as: :users_next_post
-
-    # get "users/:user_id/posts/:id/comments/new" => "comments#new", as: :comments_new
-    # post "users/:user_id/posts/:id/comments/new" => "comments#create", as: :comments_create
-
-    # get "users/:user_id/posts/:id/likes/new" => "likes#create", as: :likes_create
-    
-# end
