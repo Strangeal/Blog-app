b6567cc91b1b3076bd51d6fdc3a62b6136555ba0
strangeal <bajerijustice@gmail.com>
2023-02-01 01:24:16 -0800

    Fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email])
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :bio, :email, :password, :password_confirmation, :current_password)
     end
