e93e48f22d53aade338112bd7ceaeff3bd5470d4
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-02 19:11:17 +0100

    Fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
-   # User::Roles
+  # User::Roles
   # The available roles
   ROLES = %i[admin default].freeze
 
@@ -22,7 +22,6 @@ class User < ApplicationRecord
   #   true
   # end
 
-  
   def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
