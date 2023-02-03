00aa925ad5acc5bfe56e9f53eea7cb44d2ebe0d0
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-03 15:55:35 +0100

    Update userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+
+    render json: @users
   end
 
   def show
     @user = User.find(params[:id])
+    render json: @user
   end
 end
